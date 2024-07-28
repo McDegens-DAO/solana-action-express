1ca799ea1a82bb1be1a5cb527dd7c1a91b4ea8e2
SolDapper <soldapper.com@gmail.com>
2024-07-28 05:34:21 -0400

    modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -4,14 +4,20 @@
 // server settings
 var host = "http://localhost"; // http://localhost or your live domain
 var auto = "donate-usdc-config"; // dial.to test window : set false for prod
-var rules = {"rules":[
-{"pathPattern":"/donate-sol-config","apiPath":host+"/donate-sol-config"},
-{"pathPattern":"/donate-usdc-config","apiPath":host+"/donate-usdc-config"}
-]}; // used with auto_open for localhost dev
 // server settings
 // *********************************************************************************
 
 // *********************************************************************************
+// localhost dev actions.json rules 
+var rules = {"rules":[
+{"pathPattern":"/donate-sol-config","apiPath":host+"/donate-sol-config"},
+{"pathPattern":"/donate-usdc-config","apiPath":host+"/donate-usdc-config"}
+]};
+// localhost dev actions.json rules 
+// *********************************************************************************
+
+// *********************************************************************************
+// no edit
 if(host.includes("localhost")){host=host+":3000";}
 var rpc_file = "rpcs/rpcs.json"; // move to server root for prod
 var rpc_id = 0; // 0 = first rpc url from the file above
@@ -19,4 +25,5 @@ import fs from 'fs';
 var rpcs=JSON.parse(fs.readFileSync(rpc_file).toString());
 var rpc=rpcs[rpc_id].url;
 export var host, rpc_file, rpc_id, auto, protocol, rpcs, rpc, rules;
+// no edit
 // *********************************************************************************
