ac5ae86179f514743beaf1638c59e8ebd652dd13
SolDapper <soldapper.com@gmail.com>
2024-08-04 07:04:14 -0400

    modified:   src/actions/donation_sol.js

--- src/actions/donation_sol.js
+++ src/actions/donation_sol.js
@@ -5,13 +5,13 @@ import {rpc,host} from '../config.js';
 import {PublicKey,SystemProgram} from "@solana/web3.js";
 import mcbuild from '../mcbuild/mcbuild.js';
 import Express from 'express';
-var donation_sol = Express.Router();
+const donation_sol = Express.Router();
 // *********************************************************************************
 
 // *********************************************************************************
 // sol donation config
 donation_sol.get('/donate-sol-config',(req,res)=>{
-  let obj = {}
+  const obj = {}
   obj.icon = "https://mcdegen.xyz/images/pfp-416.png";
   obj.title = "Donate SOL to McDegens DAO";
   obj.description = "Enter SOL amount and click Send";
@@ -31,7 +31,7 @@ donation_sol.get('/donate-sol-config',(req,res)=>{
       }
     ]
   }
-  res.send(JSON.stringify(obj));
+  res.json(obj);
 });
 // *********************************************************************************
 
@@ -53,31 +53,40 @@ donation_sol.route('/donate-sol-build').post(async function(req,res){
   }
 
   if(error === true){
-    res.status(400);
-    res.send(JSON.stringify({"message":message}));
+    res.status(400).json({"message":message});
   }
   else{
-    // create instructions
-    let lamports = req.query.amount * 1000000000;
-    let from = new PublicKey(req.body.account);
-    let to = new PublicKey("GUFxwDrsLzSQ27xxTVe4y9BARZ6cENWmjzwe8XPy7AKu");
-    let donateIx = SystemProgram.transfer({fromPubkey:from, lamports:lamports, toPubkey:to});
-    // build transaction
-    let _tx_ = {};
-    _tx_.rpc = rpc;                     // string : required
-    _tx_.account = req.body.account;    // string : required
-    _tx_.instructions = [ donateIx ];   // array  : required
-    _tx_.signers = false;               // array  : default false
-    _tx_.serialize = true;              // bool   : default false
-    _tx_.encode = true;                 // bool   : default false
-    _tx_.table = false;                 // array  : default false
-    _tx_.tolerance = 2;                 // int    : default 1.1    
-    _tx_.compute = false;               // bool   : default true
-    _tx_.fees = false;                  // bool   : default true : helius rpc required when true
-    _tx_.priority = req.query.priority; // string : VeryHigh,High,Medium,Low,Min : default Medium
-    let tx = await mcbuild.tx(_tx_);    // package the tx
-    tx.message = "You sent "+req.query.amount+" SOL!";
-    res.send(JSON.stringify(tx));       // output
+
+    try{
+
+      // create instructions
+      const lamports = req.query.amount * 1000000000;
+      const from = new PublicKey(req.body.account);
+      const to = new PublicKey("GUFxwDrsLzSQ27xxTVe4y9BARZ6cENWmjzwe8XPy7AKu");
+      const donateIx = SystemProgram.transfer({fromPubkey:from, lamports:lamports, toPubkey:to});
+      
+      // build transaction
+      const _tx_ = {};
+      _tx_.rpc = rpc;                     // string : required
+      _tx_.account = req.body.account;    // string : required
+      _tx_.instructions = [ donateIx ];   // array  : required
+      _tx_.signers = false;               // array  : default false
+      _tx_.serialize = true;              // bool   : default false
+      _tx_.encode = true;                 // bool   : default false
+      _tx_.table = false;                 // array  : default false
+      _tx_.tolerance = 1.2;               // int    : default 1.1    
+      _tx_.compute = false;               // bool   : default true
+      _tx_.fees = false;                  // bool   : default true : helius rpc required when true
+      _tx_.priority = req.query.priority; // string : VeryHigh,High,Medium,Low,Min : default Medium
+      const tx = await mcbuild.tx(_tx_);  // package the tx
+      tx.message = "You sent "+req.query.amount+" SOL!";
+      res.json(tx); // output
+
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: err.message });
+    }
+
   }
 
 });
