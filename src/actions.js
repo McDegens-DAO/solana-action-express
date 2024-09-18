b16c09f755a36011258a3b11740262a6dcb588f5
SolDapper <soldapper.com@gmail.com>
2024-09-17 20:50:42 -0400

    modified:   src/actions.js

--- src/actions.js
+++ src/actions.js
@@ -25,7 +25,8 @@ app.options('*', cors(
 ));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*'); 
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Encoding, Accept-Encoding, X-Accept-Action-Version, X-Accept-Blockchain-Ids');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS');
   // res.setHeader('Access-Control-Expose-Headers', 'X-Action-Version, X-Blockchain-Ids'),
   // res.setHeader('X-Blockchain-Ids', 'solana:5eykt4UsFv8P8NJdTREpY1vzqKqZKvdp');
